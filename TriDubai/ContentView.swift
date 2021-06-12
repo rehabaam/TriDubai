7672c9ca2b27e68a7053d482577da0a3feab75ef
Ahmad Samra <rehabaam@gmail.com>
2021-06-12 21:34:26 +0300

    Initial Commit

---
+++ TriDubai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TriDubai
+//
+//  Created by Ahmad Samra on 12.6.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriDubai/TriDubaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriDubaiApp.swift
+//  TriDubai
+//
+//  Created by Ahmad Samra on 12.6.2021.
+//
+
+import SwiftUI
+
+@main
+struct TriDubaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
